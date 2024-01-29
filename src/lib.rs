7b8d54406d8bce8c3466389f4c42750fac3466cb
Troxsoft <troxz6@outlook.com>
2024-01-29 00:15:03 -0500

    add re-exports

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,15 @@
 pub mod draw;
 pub mod terminal;
+pub mod utils;
 pub use crossterm::cursor::SetCursorStyle;
 pub use crossterm::event;
 pub use crossterm::style::Color;
-pub mod utils;
+pub use draw::{
+    DrawCommunicator::{DrawCommunicator, DrawHandler},
+    DrawTerminal::DrawTerminal,
+    Text::Text,
+};
+pub use terminal::{
+    run, CursorConfig, DrawObjectTrait, DrawTrait, Terminal, TerminalsEvents, Vector2,
+};
+pub use utils::*;
